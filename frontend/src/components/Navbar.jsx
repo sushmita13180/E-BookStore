85e39066592eca53c6e6466342cd06f41edc8787
Sushmita Gupta <sushmita13180@gamil.com>
2025-02-15 22:35:18 +0530

    a

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -68,7 +68,8 @@ function Navbar() {
       <li><Link to="/about">About</Link></li>
       </ul>
     </div>
-    <a className=" text-xl md:text-4xl font-semibold hover:cursor-pointer ">E-Book Store</a>
+    {/* <a href="/"><img src="./logo1.png" alt=""  className='w-28'/></a> */}
+    <a className=" text-2xl md:text-5xl font-semibold hover:cursor-pointer ">E-Book Store</a>
   </div>
   <div className='navbar-end space-x-3'>
   <div className="navbar-center hidden lg:flex">
@@ -81,7 +82,7 @@ function Navbar() {
   </div>
   <div className='hidden md:block'>
   <label className="border rounded-md px-3 py-2 flex items-center gap-2">
-  <input type="text" className="grow outline-none px-1 rounded-md w-[120px] " placeholder="Search" />
+  <input type="text" className="grow outline-none px-1 rounded-md " placeholder="Search" />
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
@@ -99,24 +100,24 @@ function Navbar() {
   <input type="checkbox" className="theme-controller" value="synthwave" />
 
   {/* sun icon */}
-  {/* <svg
+  <svg
     className="swap-off h-8 w-8 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     onClick={()=>settheme(theme==="light"?"light":"dark")}>
     <path
       d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-  </svg> */}
+  </svg>
 
   {/* moon icon */}
-  {/* <svg
+  <svg
     className="swap-on h-8 w-8 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     onClick={()=>settheme(theme==="dark"?"dark":"light")}>
     <path
       d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-  </svg> */}
+  </svg>
 </label>
  {authUser ? <Logout /> :  <div className="">
     <a className="bg-black text-white rounded-md px-3 py-2 cursor-pointer hover:bg-slate-300 hover:text-black duration-300" onClick={()=>document.querySelector('#my_modal_3').showModal()}>Login</a>
