92565ab435eda4f628500f13b2347ff8f985060c
Sushmita Gupta <sushmita13180@gamil.com>
2025-02-15 22:31:16 +0530

    q

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ function Navbar() {
       <div className="navbar ">
   <div className="navbar-start">
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost md:hidden ml-[-50px]">
+      <div tabIndex={0} role="button" className="btn btn-ghost md:hidden ml-[-50px] md:ml-0">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5"
